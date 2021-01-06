5e5daced3b760be251f8e38139dab258c6ce9aae
George Flores <georgeflores886@gmail.com>
2021-01-06 14:31:16 -0500

    added comments

--- Day-1-Expanding-Cards/script.js
+++ Day-1-Expanding-Cards/script.js
@@ -1,12 +1,16 @@
+// Grabs all the classes of panel in variable
 const panels = document.querySelectorAll('.panel')
 
+//loops through each panel in the NodeList
 panels.forEach((panel) => {
+    //on click listener
     panel.addEventListener('click', () => {
         removeActiveClasses()
         panel.classList.add('active')
     })
 })
 
+//remove active class from panel
 function removeActiveClasses(){
     panels.forEach((panel)=> {
         panel.classList.remove('active')
