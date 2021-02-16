0bdb28d8bd76caea034f4719337f65d580f658a0
George Flores <georgeflores886@gmail.com>
2021-02-16 11:01:40 -0500

    added overflow, need to adjust note style

--- Day-33-Notes-App/script.js
+++ Day-33-Notes-App/script.js
@@ -10,7 +10,7 @@ if(notes) {
 function addNewNote(text = '') {
     const note = document.createElement('div')
     note.classList.add('note')
-    note.innerHTML =`
+    note.innerHTML = `
     <div class="tools">
         <button class="edit"><i class="fas fa-edit"></i></button>
         <button class="delete"><i class="fas fa-trash-alt"></i></button>
